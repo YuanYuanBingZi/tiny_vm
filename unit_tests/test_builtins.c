cd35fe7062be743370f8c6edf7fd1fbb846eb4ec
Tong Guan <yuanyuanbingzi@gmail.com>
2024-04-09 10:27:42 -0700

    implement the minus/times/divide operation of Int

--- unit_tests/test_builtins.c
+++ unit_tests/test_builtins.c
@@ -1,9 +1,14 @@
 /* Test cases for builtins.{c,h} */
 #include "../builtins.h"
+#include "../builtins.c"
 #include <assert.h>
 
 void test_Int() {
-    obj_ref i = int_literal(42);
-    assert(i->fields)
+    obj_ref i = int_literal((char *)42);
+    assert(i->fields);
+}
+
+int main(){
+    test_Int();
 }
 
