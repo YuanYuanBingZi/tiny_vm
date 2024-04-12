4ead5168f295f9407cba52f3fe95a0e9485c12e9
Tong Guan <yuanyuanbingzi@gmail.com>
2024-04-12 12:54:31 -0700

    fix the bug of test_builtions.c,leave the test code unfinished

--- unit_tests/test_builtins.c
+++ unit_tests/test_builtins.c
@@ -1,11 +1,18 @@
 /* Test cases for builtins.{c,h} */
 #include "../builtins.h"
-#include "../builtins.c"
+#include "../vm_state.h"
+#include "../vm_ops.h"
 #include <assert.h>
 
 void test_Int() {
-    obj_ref i = int_literal((char *)42);
+    obj_ref i = new_int(42);
     assert(i->fields);
+    vm_frame_push_word((vm_Word) {.obj = i});
+    obj_ref j = new_int(1);
+    assert(j->fields);
+    vm_frame_push_word((vm_Word) {.obj = j});
+
+
 }
 
 int main(){
